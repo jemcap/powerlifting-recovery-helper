1ffa08479123e3589d2b34f7a8aa49da071e8764
Joshua. C <76528955+jemcap@users.noreply.github.com>
2024-11-28 19:39:09 +0000

    feature: Implemented password comparing logic to authenticate registered users + generate JWT token upon successful authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,8 +5,23 @@ import User from "../models/userModel.js";
 // @desc    Authenticate user — JWT token
 // route    POST /api/users/auth
 // @access  Public
-const authUser = expressAsyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User authenticated" });
+const authUser = expressAsyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPasswords(password))) {
+      generateToken(res, user.id);
+      const savedUser = await user.save();
+      return res.status(201).json(savedUser);
+    }
+    return res.status(401).send({ error: "Invalid email or password." });
+  } catch (error) {
+    next(error);
+  }
+
+  // res.status(200).json({ message: "User authenticated" });
 });
 
 // @desc    Register user
@@ -30,7 +45,6 @@ const registerUser = expressAsyncHandler(async (req, res, next) => {
       password,
     });
     if (newUser) {
-      generateToken(res, newUser.id);
       const savedUser = await newUser.save();
       return res.status(201).json(savedUser);
     }
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -37,5 +37,11 @@ userSchema.pre("save", async function (next) {
   }
 });
 
+// Compare hashed password with password passed in
+
+userSchema.methods.matchPasswords = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
 const User = mongoose.model("User", userSchema);
 export default User;
